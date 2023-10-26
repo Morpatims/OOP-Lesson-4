6c2f1a1faed59765d71d5095cec4daf5716030f0
morpatims <kverteletski@mail.ru>
2023-10-26 15:24:05 +0300

    Homework
    Improved the Box and Program method

--- src/main/java/ru/geekbrains/lesson4/homework/Box.java
+++ src/main/java/ru/geekbrains/lesson4/homework/Box.java
@@ -2,8 +2,30 @@ package ru.geekbrains.lesson4.homework;
 
 import java.util.ArrayList;
 
-public class Box <T extends Fruit> {
+public class Box<T extends Fruit> {
 
     private ArrayList<T> fruits = new ArrayList<>();
 
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public float getWeight() {
+        if (fruits.isEmpty()) {
+            return 0.0f;
+        }
+        return fruits.size() * fruits.get(0).getWeight();
+    }
+
+    public boolean compare(Box<?> otherBox) {
+        return Math.abs(getWeight() - otherBox.getWeight()) < 0.0001;
+    }
+
+    public void transferFruitsTo(Box<T> otherBox) {
+        if (this == otherBox) {
+            return;
+        }
+        otherBox.fruits.addAll(fruits);
+        fruits.clear();
+    }
 }
--- src/main/java/ru/geekbrains/lesson4/homework/Program.java
+++ src/main/java/ru/geekbrains/lesson4/homework/Program.java
@@ -2,26 +2,34 @@ package ru.geekbrains.lesson4.homework;
 
 public class Program {
 
-    /**
-     Домашняя работа, задача:
-     ========================
-
-     a. Даны классы Fruit, Apple extends Fruit, Orange extends Fruit;
-     b. Класс Box, в который можно складывать фрукты. Коробки условно сортируются по типу фрукта,
-     поэтому в одну коробку нельзя сложить и яблоки, и апельсины;
-     c. Для хранения фруктов внутри коробки можно использовать ArrayList;
-     d. Сделать метод getWeight(), который высчитывает вес коробки, зная вес одного фрукта и их количество:
-     вес яблока – 1.0f, апельсина – 1.5f (единицы измерения не важны);
-     e. Внутри класса Box сделать метод compare(), который позволяет сравнить текущую коробку с той, которую
-     подадут в compare() в качестве параметра. true – если их массы равны, false в противоположном случае.
-     Можно сравнивать коробки с яблоками и апельсинами;
-     f. Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую.
-     Помним про сортировку фруктов: нельзя яблоки высыпать в коробку с апельсинами.
-     Соответственно, в текущей коробке фруктов не остается, а в другую перекидываются объекты, которые были в первой;
-     g. Не забываем про метод добавления фрукта в коробку.
-     */
     public static void main(String[] args) {
         Box<Orange> orangeBox = new Box<>();
         Box<Apple> appleBox = new Box<>();
+
+        orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        appleBox.addFruit(new Apple());
+
+        System.out.println("Вес коробки с апельсинами: " + orangeBox.getWeight());
+        System.out.println("Вес коробки с яблоками: " + appleBox.getWeight());
+
+        Box<Orange> anotherOrangeBox = new Box<>();
+        System.out.println("Сравнение коробки с апельсинами и пустой коробкой: " + orangeBox.compare(anotherOrangeBox));
+
+        orangeBox.transferFruitsTo(anotherOrangeBox);
+        System.out.println("Вес первой коробки после пересыпания: " + orangeBox.getWeight());
+        System.out.println("Вес второй коробки после пересыпания: " + anotherOrangeBox.getWeight());
+
+        appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        
+        System.out.println("Вес коробки с яблоками: " + appleBox.getWeight());
+
+        Box<Apple> anotherAppleBox = new Box<>();
+        System.out.println("Сравнение коробки с яблоками и пустой коробкой: " + appleBox.compare(anotherAppleBox));
+
+        appleBox.transferFruitsTo(anotherAppleBox);
+        System.out.println("Вес первой коробки после пересыпания: " + appleBox.getWeight());
+        System.out.println("Вес второй коробки после пересыпания: " + anotherAppleBox.getWeight());
     }
 }
